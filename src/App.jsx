ca8498870d47d2cddf17201cc2e9bf885a1fe02c
lucasssantos94 <lucas.ssantos94@outlook.com>
2024-02-22 14:47:37 -0300

    build

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cronometo from "./components/Cronometro"
+
+function App() {
+
+
+  return (
+    <>
+      <Cronometo />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cronometro/index.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import './style.css'
+
+export default function Cronometo() {
+    const [second, setSecond] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [hour, setHour] = useState(0);
+    const [saveInterval, setSaveInterval] = useState();
+
+    if (second === 60) {
+        setSecond(0)
+        setMinute((minute) => ++minute)
+    }
+
+    if (minute === 60) {
+        setMinute(0)
+        setHour((hour) => ++hour)
+    }
+
+    function start() {
+        const interval = setInterval(() => {
+            setSecond((second) => ++second)
+        }, 1000);
+
+        setSaveInterval(interval)
+    }
+
+    function pause() {
+        clearInterval(saveInterval)
+    }
+
+    function stop() {
+        setSecond(0);
+        setMinute(0);
+        setHour(0);
+        setTimeout(clearInterval(saveInterval))
+    }
+
+    return (
+        <div className="container">
+            <h1>
+                {`${hour >= 10 ? hour : `0${hour}`}:${minute >= 10 ? minute : `0${minute}`}:${second >= 10 ? second : `0${second}`}`}
+            </h1>
+
+            <div className="buttons">
+                <button onClick={start} className="start">Start</button>
+                <button onClick={pause} className="pause">Pause</button>
+                <button onClick={stop} className="stop">Stop</button>
+            </div>
+        </div>
+    )
+}
